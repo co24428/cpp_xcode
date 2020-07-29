bcfa4c133f11a87b1a58eb03e88353a6d30ded1d
ihwan <ihwan1236@gmail.com>
2020-07-29 15:22:52 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 김이환 on 2020/07/29.
+//  Copyright © 2020 김이환. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
