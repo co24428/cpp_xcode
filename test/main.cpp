4b70cc7600c584bae2d87d1cd6f39ccc62b87bab
ihwan <ihwan1236@gmail.com>
2020-07-29 15:51:40 +0900

    success connect Xcode & git

--- test/main.cpp
+++ test/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::cout << "From ihwan\n";
+    std::cout << "success git connect!\n";
     return 0;
 }
